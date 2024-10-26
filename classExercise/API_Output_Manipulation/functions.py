e05d286943fd611ed0b36d9c57e61ddf1c8ecf0b
maidormo <elia.gatti01@gmail.com>
2024-10-26 16:42:11 +0200

    Added a possible exercise and made sure it's working

--- classExercise/API_Output_Manipulation/functions.py
+++ classExercise/API_Output_Manipulation/functions.py
@@ -1,16 +1,45 @@
-from FastAPI import FileResponse
-from PythonGUI.APIs import speech_to_text
+import os, sys
+from fastapi.responses import FileResponse
+import neuralspace as ns
+from typing import Dict, Any
 
 def extract_song(song: FileResponse, path: str):
     with open(f"{path}/song.mp3", "wb") as f:
-        f.write(song)
+        f.write(song.content)
 
 def speech_to_text(path: str):
-    vai = speech_to_text.initialize_voice_ai()
-    job_id = speech_to_text.create_transcription_job(vai, f"{path}/song.mp3")
+    vai = initialize_voice_ai()
+    job_id = create_transcription_job(vai, f"{path}/song.mp3")
     result = vai.poll_until_complete(job_id)
-    return speech_to_text.extract_transcription_words(result)
+    return extract_transcription_words(result)
 
 def download_image(image_url: str, path: str):
     image_save_path = f"{path}/images/downloaded_image.png"
     download_image.download_image(image_url, image_save_path)
+
+#-----------------------------------------------------------------------------------------------------------------------
+# API key for STT
+STT_API_KEY: str = "sk_a58a31d32bed7896a38ea55fa93fe8a1d709851e37e38c67e163c6004c9a0186"
+
+# Initialize VoiceAI with API key
+def initialize_voice_ai() -> ns.VoiceAI:
+    return ns.VoiceAI(api_key=STT_API_KEY)
+
+# Setup job configuration
+def create_transcription_job(vai: ns.VoiceAI, filename: str) -> str:
+    config= {
+        'file_transcription': {
+            'language_id': 'en',
+            'mode': 'advanced',
+        }
+    }
+    job_id = vai.transcribe(file=filename, config=config)
+    print(f'Created job: {job_id}')
+    return job_id
+
+# Extract words from transcription result
+def extract_transcription_words(result: Dict[str, Any]) -> str:
+    timestamps = result['data']['result']['transcription']['channels']['0']['timestamps']
+    print(result)
+    words = [entry['word'] for entry in timestamps]
+    return " ".join(words)
--- classExercise/composition_exercise.py
+++ classExercise/composition_exercise.py
@@ -1,6 +1,10 @@
+import sys
+import os
 from API_Output_Manipulation import functions
-from PythonGUI.APIs import download_image, image_search, list_music, select_music
+from fastapi.responses import FileResponse
 import pathlib
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../PythonGUI")))
+from APIs import download_image, image_search, list_music, select_music
 
 # You have available the following functions
 # ----------------------------------------------------------------------------------------------------------------------------------------------
@@ -10,14 +14,13 @@ import pathlib
 # select_music.importSong()                 Given a string with the name of the song, returns a FileResponse that contains a file with that song
 
 # functions.download_image()                Given an image url and a path, download that image in the specified path
-# functions.extract_song()                  Given a FileResponse and a path, downloads the image in the specified path
+# functions.extract_song()                  Given a FileResponse and a path, downloads the song in the specified path
 # functions.speech_to_text()                Given the path to a song, extracts its lyrics
 # ----------------------------------------------------------------------------------------------------------------------------------------------
 
 # And the following constants which are used to specify the paths of songs and images
-CURRENT_PATH = pathlib.Path(__file__).parent.resolve().as_posix()
-SONG_PATH = CURRENT_PATH+"/song.mp3"
-IMAGE_PATH = CURRENT_PATH+"/images/downloaded_image.png"
+SONG_PATH = pathlib.Path(__file__).parent.resolve().as_posix()
+IMAGE_PATH = SONG_PATH + "/images/downloaded_image.png"
 
 # Write a main function that performs the following composition:
 # List songs -> Import 1st song available -> Extract lyrics from that song -> Perform a google search using the lyrics ->
@@ -25,6 +28,21 @@ IMAGE_PATH = CURRENT_PATH+"/images/downloaded_image.png"
 
 def main():
 
-    # WRITE HERE
+    # SOLUTION
+    song_list = list_music.listAvailableMusic()
+    print(song_list)
+    selected_song: FileResponse = select_music.importSong(song_list[0])
+    functions.extract_song(selected_song, SONG_PATH)
+    song_text = functions.speech_to_text(SONG_PATH)
+    print(song_text)
+    url = image_search.perform_image_search(song_text)
+    print(url)
+    download_image.download_image(url,IMAGE_PATH)
+    print("finito, so nice!")
 
-    return 0
+    # WRITE HERE
+   
+
+
+if __name__ == "__main__":
+    main()
