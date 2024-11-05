ca0976aecc65fa0afe0221f75144cf9429e8d4f6
maidormo <elia.gatti01@gmail.com>
2024-11-05 20:20:09 +0100

    Changed search API to duck duck go

--- PythonGUI/APIs/image_search.py
+++ PythonGUI/APIs/image_search.py
@@ -1,36 +1,18 @@
-import requests
-
-
-IMG_SEARCH_API_KEY= "AIzaSyAevW4pAAiZRODwx0nlhTGjVNZaJyB1Yl8"
-CX= "f0e530b997279403d"
-
-
-# Define endpoint
-GOOGLE_SEARCH_URL = "https://www.googleapis.com/customsearch/v1"
+from duckduckgo_search import DDGS
 
 
 def perform_image_search(query: str) -> (str | None):
-    """ Perform Google image search based on transcribed text """
-    params = {
-        "key": IMG_SEARCH_API_KEY,
-        "cx": CX,
-        "q": query,
-        "searchType": "image",
-        "num": 1,
-        "fileType": "jpg"
-    }
-    response = requests.get(GOOGLE_SEARCH_URL, params=params)
-    if response.status_code == 200:
-        search_results = response.json()
-        print(search_results)
-        if 'items' in search_results and search_results['items']:
-            return search_results['items'][0]['link']
-        else:
-            print("No image results found.")
-            return None
-    else:
-        print(f"Failed to fetch search results. Status code: {response.status_code}")
-        return None
+    """Perform DuckDucKGo image search based on transcribed text"""
+    results = None
+    try:
+        results = DDGS().images(
+                keywords=query,
+                region="wt-wt",
+                safesearch="moderate",
+                max_results=1,
+            )
+    except:
+        print("Error in requesting the image")
 
-
-
+    return results
+    
